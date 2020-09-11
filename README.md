0374eef20ef59d83aa7c1b8977c85a848a3c9f59
Larry Cessna <larrybcessna@gmail.com>
2020-09-11 16:14:16 -0700

    Testing random upper and lower case retrieval. Seems to work.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # hw3-password-generator
+
+- Initial commit. Pulled docs over.
+- Testing js function to create lower and upper case letters is working.
--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-// Assignment Code
+// Testing generating random upper and lower case letters
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+} 
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+} 
+console.log("getRandomLower() = ", getRandomLower());
+console.log("getRandomUpper() = ", getRandomUpper());
+
+
+/*// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -6,25 +17,25 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  var generatePassword = function(){
-    var length = prompt("How long does your password need to be?");
-    console.log(length);
-    
-  }
+  passwordText.value = password;
 
-
-//TODO: WHEN I click the button to generate a password THEN I am presented with a series of prompts for password criteria
-//TODO: WHEN prompted for password criteria THEN I select which criteria to include in the password
-//TODO: WHEN prompted for the length of the password THEN I choose a length of at least 8 characters and no more than 128 characters
-//TODO: WHEN prompted for character types to include in the password THEN I choose lowercase, uppercase, numeric, and/or special characters
-//TODO: WHEN I answer each prompt THEN my input should be validated and at least one character type should be selected
-//TODO: WHEN all prompts are answered THEN a password is generated that matches the selected criteria
-//TODO: WHEN the password is generated THEN the password is either displayed in an alert or written to the page
-
-passwordText.value = password;
+  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+*/
 
-// console.log(String.fromCharCode((Math.floor(Math.random() + 26) + 97)));
+
+
+//TODO: WHEN I click the button to generate a password THEN I am presented with a series of prompts for password criteria
+
+//TODO: WHEN prompted for password criteria THEN I select which criteria to include in the password
+//TODO: WHEN prompted for the length of the password THEN I choose a length of at least 8 characters and no more than 128 characters
+//TODO: WHEN prompted for character types to include in the password THEN I choose lowercase, uppercase, numeric, and/or special characters
+
+
+
+//TODO: WHEN I answer each prompt THEN my input should be validated and at least one character type should be selected
+//TODO: WHEN all prompts are answered THEN a password is generated that matches the selected criteria
+//TODO: WHEN the password is generated THEN the password is either displayed in an alert or written to the page
